e40597ec00d488fe7f505c297bf50093fae313db
Aymen Leknouche <dev.aymenleknouche@gmail.com>
2024-07-01 21:42:53 +0200

    seed

--- database/seeders/LessonTagSeeder.php
+++ database/seeders/LessonTagSeeder.php
@@ -2,17 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\LessonTag;
 
 class LessonTagSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         LessonTag::factory()->count(500)->create();
